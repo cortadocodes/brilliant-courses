e02f2915695547de567dae549eb00f898bccbc1d
Marcus Lugg <marcuslugg@googlemail.com>
2018-08-28 13:23:37 +0100

    ENH: Add parents and children attributes to nodes
    Also, remove .next attribute

---
+++ computer_science_fundamentals/algorithms/strings/trie.py
@@ -0,0 +1,7 @@
+
+class trie:
+
+    def __init__(self, path=None):
+        self.name = 'Trie'
+        self.path = path
+        self.start_node =
--- computer_science_fundamentals/data_types/node.py
+++ computer_science_fundamentals/data_types/node.py
@@ -1,23 +1,35 @@
 class Node:
     """
-    Create a singly- or doubly-linked node that can be the foundation for a linked list.
+    Create a singly- or doubly-linked node that can be the foundation for a linked list or trie.
 
     :param any value: data for node to store
     :param bool doubly_linked: doubly-linked if True, singly-linked if False
     """
     def __init__(self, value=None, is_doubly_linked=False):
         self.value = value
-        self.next = None
+        self.children = {}
+        self.parents = {}
+        self.is_doubly_linked = is_doubly_linked
 
-        if isinstance(is_doubly_linked, bool):
-            if is_doubly_linked:
-                self.previous = None
-                self.type = 'doubly-linked'
-            else:
-                self.type = 'singly-linked'
-        else:
+        if not isinstance(is_doubly_linked, bool):
             raise TypeError('is_doubly_linked should be boolean.')
 
     def __repr__(self):
-        representation = '{} Node: {}'.format(self.type, self.value)
+        if self.is_doubly_linked:
+            type = 'doubly-linked'
+        else:
+            type = 'singly-linked'
+        representation = '{} Node: {}'.format(type, self.value)
         return representation
+
+    def connect_child_nodes(self, child_nodes):
+        for child in child_nodes:
+            self.children[child.value] = child
+            if child.is_doubly_linked:
+                child.parents[self.value] = self
+
+    def connect_parent_nodes(self, parent_nodes):
+        for parent in parent_nodes:
+            parent.children[self.value] = self
+            if self.is_doubly_linked:
+                self.parents[parent.value] = parent
